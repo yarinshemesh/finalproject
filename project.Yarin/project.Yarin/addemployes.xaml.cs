f025184474510ffa338e03bdd20a4f2791d42c8a
תקשוב אשדוד <Owner@LAB37.MEKIF11.LOCAL>
2020-02-09 13:04:14 +0200

    data base

---
+++ project.Yarin/project.Yarin/addemployes.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace project.Yarin
+{
+    /// <summary>
+    /// Interaction logic for addemployes.xaml
+    /// </summary>
+    public partial class addemployes : Window
+    {
+        public addemployes()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_button(object sender, RoutedEventArgs e)
+        {
+            addemployes d = new addemployes();
+            this.Close();
+        }
+    }
+}
